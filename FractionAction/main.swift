3f206642dc40b02dd0f5d1156151c94a280b870f
Ryan Bowler <rbowler@lcs.on.ca>
2018-04-28 10:29:41 -0400

    Close but not quite yet

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -11,10 +11,79 @@ import Foundation
 // INPUT
 // Collect and filter user input here
 
+// Make sure numerator is acceptable
+var numerator = 0
+
+while 1 == 1 {
+    
+    print("Numerator?")
+
+    // Make sure input is not nil
+    guard let givenInput = readLine() else {
+        continue
+    }
+    
+    // Make sure numerator is an integer
+    guard let givenNumerator = Int(givenInput) else {
+        continue
+    }
+    
+    // Is the input between 1 and 32500
+    if givenNumerator < 0 {
+        continue
+    }
+    
+    numerator = givenNumerator
+    break
+}
+
+// Make sure denominator is acceptable
+var denominator = 0
+
+while 2 == 2 {
+    
+    print("Denominator?")
+    
+    // Make sure input is not nil
+    guard let givenInput = readLine() else {
+        continue
+    }
+    
+    // Make sure denominator is an integer
+    guard let givenDenominator = Int(givenInput) else {
+        continue
+    }
+    
+    // Is the input between 1 and 32500
+    if givenDenominator < 1 {
+        continue
+    }
+    
+    denominator = givenDenominator
+    break
+}
 
 // PROCESS
 // Implement the primary logic of the problem here
 
+// Find Quotient and Remainder
+let quotient = numerator / denominator
+let remainder = numerator % denominator
+
+// Check result
+// Whole number?
+if remainder == 0 {
+    print("The result is:")
+    print(quotient)
+// Negative Fraction?
+} else if numerator < denominator {
+    print("The result is:")
+    print("\(quotient)")
+// Normal Fraction
+} else {
+    print("The result is:")
+    print("\(quotient) \(remainder)/\()")
+}
 
 // OUTPUT
 // Report results to the user here
